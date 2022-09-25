23b2877d49ed5268a668de36a9ff4d065d1aa111
Kateryna Berehovych <kasia-94@ukr.net>
2022-09-25 22:08:20 +0300

    Update files

--- js/variables.js
+++ js/variables.js
@@ -1,8 +1,5 @@
 const KEY = '332ebb0c59493b862a9beffecc6f1777';
 const URL = `https://api.themoviedb.org/`;
 
-
-
-
 // EXAMPLE API
 // https://api.themoviedb.org/3/movie/550?api_key=332ebb0c59493b862a9beffecc6f1777
